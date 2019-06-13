29b8ee2cef7a2c9243594c7d6fbdb857d43dc960
Imtiaze <ahammedimtiaze78@gmail.com>
2019-06-13 08:33:57 +0600

    Boostrap Model modified for add new Item, Edit item

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -23,9 +23,9 @@
                     <div class="card-body">
                         <div class="card" >
                             <ul class="list-group list-group-flush">
-                                <li class="list-group-item">Cras justo odio</li>
-                                <li class="list-group-item">Dapibus ac facilisis in</li>
-                                <li class="list-group-item">Vestibulum at eros</li>
+                                <li class="list-group-item ourItem" data-toggle="modal" data-target="#exampleModal">Dapibus ac facilisis in</li>
+                                <li class="list-group-item ourItem" data-toggle="modal" data-target="#exampleModal">Cras justo odio</li>
+                                <li class="list-group-item ourItem" data-toggle="modal" data-target="#exampleModal">Vestibulum at eros</li>
                             </ul>
                         </div>
                     </div>
@@ -40,17 +40,18 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <h5 class="modal-title" id="title">Add New Item</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-                    ...
+                    <input type="text" placeholder="Write Item here" id="addItem" class="form-control">
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-warning" style="display:none;" id="delete">Delete</button>
+                    <button type="button" class="btn btn-primary" style="display:none;" id="saveChanges">Save changes</button>
+                    <button type="button" class="btn btn-primary" id="addButton">Add Item</button>
                 </div>
             </div>
         </div>
@@ -58,5 +59,22 @@
     
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script>
+        
+        $(document).ready(function() {
+            $('.ourItem').each(function(index, el) {
+                $(this).click(function(event) {
+                    let text = $(this).text();
+                    $('#title').text('Edit Item');
+                    $('#delete').show(200);
+                    $('#saveChanges').show(200);
+                    $('#addButton').hide(200);
+                    $('#addItem').val(text);
+                    console.log(text);
+                });
+            });
+        });
+
+    </script>
 </body>
 </html>
